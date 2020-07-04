ce82e7997a3e7cbe87f7d9f15a58aa4887463e5b
Sergio Clavijo <sergio.clavijo@wunderman.com>
2020-07-04 11:39:32 -0500

    Added basic bootstrap scaffolding and basic user auth

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/', 'PublicController@index');
 
 Route::resource('products', 'ProductController');
